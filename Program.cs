6d737f7e0b5e7c95276f6a9bb7ab4f88310662d3
Skye <exp.lunate@gmail.com>
2019-11-17 23:56:19 +0000

    Entry (TM)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace SeraphinaNET
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            MainAsync().GetAwaiter().GetResult();
+        }
+        public static async Task MainAsync() {
+            var data = throw new NotImplementedException();
+            var discord = throw new NotImplementedException(); // I don't have code completion
+            using var seraphina = new SeraphinaCore(discord, data);
+            await seraphina.Connect("seizure token from environment");
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+    }
+}
