2ca541fd7418642b0249517b1e6145803df41f20
Skye <exp.lunate@gmail.com>
2019-11-17 23:50:50 +0000

    Data with no concrete impl

---
+++ Data.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+namespace SeraphinaNET.Data {
+    public interface DataContextFactory {
+        // I hate factories but they seem to be the best way to encapsulate 
+        // hidden arguments from an external context.
+        DataContext GetContext();
+    }
+
+    public interface DataContext : IDisposable {
+        Task<ulong[]> GetPins(ulong channel);
+        Task ClearPins(ulong channel);
+        Task AddPin(ulong channel, ulong message);
+        Task RemovePin(ulong channel, ulong message);
+        Task<bool> IsPinned(ulong channel, ulong message);
+    }
+}
