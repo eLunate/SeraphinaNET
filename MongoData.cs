f59d321bb55edac00b38dad366f99b51c89b7fdc
Skye <exp.lunate@gmail.com>
2019-12-05 00:16:56 +0000

    Concrete data impl
    Also some dumb shit that's unrelated and maybe didn't need to change

--- MongoData.cs
+++ MongoData.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using MongoDB.Driver;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SeraphinaNET.Data {
     public class MongoData : DataContextFactory {
@@ -24,6 +26,10 @@ namespace SeraphinaNET.Data {
             this.db = db;
         }
 
+        // Stub Dispose method as Mongo thinks you should only have one Client.
+        // So the factory holds this and calls it a day.
+        public void Dispose() { }
+
         [BsonIgnoreExtraElements]
         private class DBChannelInfo {
             // Mongo uses _id
@@ -32,10 +38,57 @@ namespace SeraphinaNET.Data {
             [BsonElement("pins")]
             public ulong[]? Pins { get; set; }
         }
-        static MongoDataContext() { 
-            BsonClassMap.RegisterClassMap<DBChannelInfo>(); 
+
+        [BsonIgnoreExtraElements]
+        private class DBActionInfo {
+            [BsonId]
+            public ulong Id { get; set; }
+            [BsonElement("action")]
+            public int ActionId { get; set; }
+            [BsonElement("radio")]
+            public Dictionary<string, string> Radio { get; set; }
+            [BsonElement("tally")]
+            public Dictionary<string, string[]> Tally { get; set; }
         }
 
+        private class DBActionController : ActionData {
+            private readonly IMongoDatabase db;
+            private readonly DBActionInfo info;
+
+            int ActionData.ActionType => info.ActionId;
+
+            public DBActionController(IMongoDatabase db, DBActionInfo info) {
+                this.db = db;
+                this.info = info;
+            }
+
+            async Task ActionData.AddTally(ulong user, string emote) {
+                var col = db.GetCollection<DBActionInfo>("actions");
+                var filter = Builders<DBActionInfo>.Filter;
+                var update = Builders<DBActionInfo>.Update;
+                await col.UpdateOneAsync(filter.Eq("_id", info.Id), update.AddToSet($"tally.{user}", emote));
+            }
+            Task<string?> ActionData.GetRadioData(ulong user) => Task.FromResult(info.Radio.TryGetValue(user.ToString(), out var data) ? data : null);
+            Task<string[]> ActionData.GetTallyData(ulong user) => Task.FromResult(info.Tally.TryGetValue(user.ToString(), out var data) ? data : Array.Empty<string>());
+            async Task ActionData.RemoveTally(ulong user, string emote) {
+                var col = db.GetCollection<DBActionInfo>("actions");
+                var filter = Builders<DBActionInfo>.Filter;
+                var update = Builders<DBActionInfo>.Update;
+                await col.UpdateOneAsync(filter.Eq("_id", info.Id), update.Pull($"tally.{user}", emote));
+            }
+            async Task ActionData.SetRadioData(ulong user, string? data) {
+                var col = db.GetCollection<DBActionInfo>("actions");
+                var filter = Builders<DBActionInfo>.Filter;
+                var update = Builders<DBActionInfo>.Update;
+                await col.UpdateOneAsync(filter.Eq("_id", info.Id), update.Set($"radio.{user}", data));
+            }
+        }
+        static MongoDataContext() { 
+            BsonClassMap.RegisterClassMap<DBChannelInfo>();
+            BsonClassMap.RegisterClassMap<DBActionInfo>();
+        }
+
+        #region impl Pin
         public async Task AddPin(ulong channel, ulong message) {
             var col = db.GetCollection<DBChannelInfo>("channels");
             var filter = Builders<DBChannelInfo>.Filter;
@@ -46,7 +99,7 @@ namespace SeraphinaNET.Data {
         public async Task<ulong[]> GetPins(ulong channel) {
             var col = db.GetCollection<DBChannelInfo>("channels");
             var filter = Builders<DBChannelInfo>.Filter;
-            var info = await col.Find(filter.Eq("_id", channel)).SingleOrDefaultAsync();
+            var info = await col.Find(filter.Eq("_id", channel)).FirstOrDefaultAsync();
             return info?.Pins ?? Array.Empty<ulong>();
         }
         public async Task SetPins(ulong channel, ulong[] messages) {
@@ -66,9 +119,19 @@ namespace SeraphinaNET.Data {
             var update = Builders<DBChannelInfo>.Update;
             await col.UpdateOneAsync(filter.Eq("_id", channel), update.Pull("pins", message));
         }
+        #endregion
 
-        // Stub Dispose method as Mongo thinks you should only have one Client.
-        // So the factory holds this and calls it a day.
-        public void Dispose() {}
+        #region impl Action
+        public async Task<ActionData?> GetAction(ulong message) {
+            var col = db.GetCollection<DBActionInfo>("actions");
+            var filter = Builders<DBActionInfo>.Filter;
+            var data = await col.Find(filter.Eq("_id", message)).FirstAsync();
+            return (data == null || data.Id == 0) ? null : new DBActionController(db, data);
+        }
+        public async Task SetAction(ulong message, int action) {
+            var col = db.GetCollection<DBActionInfo>("actions");
+            await col.InsertOneAsync(new DBActionInfo() { Id = message, ActionId = action });
+        }
+        #endregion
     }
 }
