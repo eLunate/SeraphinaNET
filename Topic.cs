9de398e471a9ede71eca154ed06247754fe91c16
Skye <exp.lunate@gmail.com>
2019-12-05 21:11:34 +0000

    This is a really mundane service
    Not sure this is actually useful in its current state?

--- Topic.cs
+++ Topic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using SeraphinaNET.Data;
+using Discord;
 
 namespace SeraphinaNET {
     class TopicService {
@@ -9,6 +10,32 @@ namespace SeraphinaNET {
 
         public TopicService(DataContextFactory data) {
             this.data = data;
+        }
+
+        public Task<TopicData?> GetTopic(IGuildChannel channel) {
+            using var db = data.GetContext();
+            return db.GetTopicByChannel(channel.GuildId, channel.Id);
+        }
+        public Task<TopicData?> GetTopic(ulong guild, string name) {
+            using var db = data.GetContext();
+            return db.GetTopicByName(guild, name);
+        }
+        public Task<TopicData?> GetTopic(IGuild guild, string name) => GetTopic(guild.Id, name);
+        public Task<TopicData?> GetTopic(ulong guild, IEmote emote) {
+            using var db = data.GetContext();
+            var emoteStr = emote.ToString();
+            return emoteStr == null ? Task.FromResult<TopicData?>(null) : db.GetTopicByEmote(guild, emoteStr);
+        }
+        public Task<TopicData?> GetTopic(IGuild guild, IEmote emote) => GetTopic(guild.Id, emote);
+        
+        public Task AddUser(TopicData topic, IGuildUser user) {
+            var role = user.Guild.GetRole(topic.TopicRole);
+            return user.AddRoleAsync(role);
+        }
+
+        public Task RemoveUser(TopicData topic, IGuildUser user) {
+            var role = user.Guild.GetRole(topic.TopicRole);
+            return user.RemoveRoleAsync(role);
         }
     }
 }
