0283edd8f1e1ca30ed198de10ba069add9d475e2
Skye <exp.lunate@gmail.com>
2019-12-26 23:16:47 +0000

    uhhhh debug command

---
+++ Commands/User.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using SeraphinaNET.Services;
+
+namespace SeraphinaNET.Commands {
+    class UserCommands : ModuleBase<SocketCommandContext> {
+        private readonly UserService user;
+
+        public UserCommands(UserService user) {
+            this.user = user;
+        }
+
+        [Command("xp")]
+        [RequireContext(ContextType.Guild)]
+        public async Task GetXP() => await ReplyAsync((await user.GetMemberXP(Context.Guild.Id, Context.User.Id)).ToString());
+    }
+}
