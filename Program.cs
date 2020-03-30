02f5816437ac32c16d1e17ccfbe69cb2475f9837
Skye <eLunate@users.noreply.github.com>
2020-03-30 18:23:10 +0100

    Use MySQL
    Also CR/LF

--- Program.cs
+++ Program.cs
@@ -7,14 +7,16 @@ namespace SeraphinaNET
 {
     class Program
     {
-        public static void Main(string[] args) {
+        public static void Main(string[] _) {
             MainAsync().GetAwaiter().GetResult();
         }
         public static async Task MainAsync() {
-            var data = new MongoData(
-                Environment.GetEnvironmentVariable("DB_STRING") ?? throw new InvalidOperationException("DB_STRING not in environment"), 
-                Environment.GetEnvironmentVariable("DB_NAME") ?? "dev"
+            var data = new MySQLData(
+                Environment.GetEnvironmentVariable("DB_STRING") ?? throw new InvalidOperationException("DB_STRING not in environment")
             );
+            using (var ctx = data.GetContext()) {
+                Microsoft.EntityFrameworkCore.RelationalDatabaseFacadeExtensions.Migrate(ctx.Database); // Fuck that noise.
+            }
             var discord = new DiscordSocketClient(); 
             using var seraphina = new SeraphinaCore(discord, data);
             await seraphina.Connect(Environment.GetEnvironmentVariable("BOT_TOKEN") ?? throw new InvalidOperationException("BOT_TOKEN not in environment"));
